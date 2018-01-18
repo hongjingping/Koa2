6e298ad1f57bf7b9afdbff14b69e3eedbd8df9de
wikihong <hjpChinash@163.com>
2018-01-18 15:56:58 +0800

    直接从上下文中GET请求的接收0118

--- demo02.js
+++ demo02.js
@@ -2,14 +2,21 @@ const Koa = require('koa');
 const app = new Koa();
 app.use( async (ctx) => {
   let url = ctx.url;
+  // 从request中接收get请求
   let request = ctx.request;
   let req_query = request.query;
   let req_querystring = request.querystring;
 
+  // 不从request中接收get请求，从上下文中直接获取get请求
+  let ctx_query = ctx.query;
+  let ctx_querystring = ctx.querystring;
+
   ctx.body={
     url,
     req_query,
-    req_querystring
+    req_querystring,
+    ctx_query,
+    ctx_querystring
   }
 })
 
