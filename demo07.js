f07df6ce425187530c47281d663eb3c1eb540d9d
wikihong <hjpChinash@163.com>
2018-01-19 11:33:27 +0800

    Koa-router中间件(2)-加路由层级0119

---
+++ demo07.js
@@ -0,0 +1,23 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router({
+  prefix: '/wikiHong'
+});
+
+router
+  .get('/', (ctx, next) => {
+    ctx.body = 'hello, wikiHong';
+  })
+  .get('/todo', (ctx, next) => {
+    ctx.body = 'todo page';
+  })
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log('starting at port 3000');
+});
